308554391f655bd2c855dff364b6ab8deb0fdc5f
Railson <railsonmartins1970@gmail.com>
2023-10-27 08:52:34 -0300

    Fix intellisense problem

--- includes/protocol/protocol.h
+++ includes/protocol/protocol.h
@@ -7,10 +7,12 @@
 class Protocol: public IProtocol
 {
 private:
-    
+    uint8_t operation_mode;
 public:
     Protocol(/* args */);
     ~Protocol();
+    uint8_t get_operation_mode();
+    void set_operation_mode(uint8_t operation_mode);
 };
 
 
